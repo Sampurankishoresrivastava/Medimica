b54228849d0a2468d9c1b550d5e582fd3d6f352d
Sampuran Kishore Srivastava <sampuran01122001@gmail.com>
2022-09-17 18:34:23 +0530

    intiial commit

---
+++ src/Components/Header.js
@@ -0,0 +1,18 @@
+import Box from '@mui/material/Box';
+
+const Header = () => {
+  return (
+    <Box display='flex' width='70%' margin='auto' gap='2rem'>
+    <Box>Logo</Box>
+    <Box>
+    
+
+    </Box>
+    <Box>Stock</Box>
+    <Box>Sign in</Box>
+    <Box>Sign up</Box>
+    </Box>
+  )
+}
+
+export default Header
